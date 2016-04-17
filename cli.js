37ccee95d2c27dceb40264a10fea8a2068aec03a
Andreas Gillström <andreasgillstrom@gmail.com>
2016-04-17 15:40:19 +0200

    Improve the looks of CLI
    Fixes #1.

--- cli.js
+++ cli.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 'use strict';
+const leftPad = require('left-pad');
 const meow = require('meow');
 const osxInfo = require('./');
 
-meow(`
+const cli = meow(`
 	Usage
 	  $ osx-info
+
+	Options
+	  --json  Output the result as JSON
 `);
 
-osxInfo().then(data => console.log(data));
+osxInfo().then(data => {
+	if (cli.flags.json) {
+		console.log(JSON.stringify(data));
+		process.exit(0);
+	}
+
+	Object.keys(data).forEach(x => {
+		console.log(`${leftPad(x, 12)}: ${data[x]}`);
+	});
+});
